7e15bca50d6a9c54b806a0ff8b1dbcd66da32f1e
Alejandro Chitiva Castillo <83592808+AlejandroChitivaC@users.noreply.github.com>
2022-06-11 17:25:05 -0500

    sessionStorage partially functionally with frontend

---
+++ src/main/webapp/scripts/perfilArtista.js
@@ -0,0 +1,15 @@
+let info_p = document.getElementById("#info-p");
+let data = sessionStorage.getItem("newUser");
+let userData = JSON.parse(data);
+
+if (userData.rol === "Artista") {
+  info_p.innerHTML = `<div>
+  <img style="margin-left:100px" class="profile-pic" src=${userData.imagen} alt="userPicture"/>
+  <h5 style="color:white;font-weight:normal">${userData.name}</h5>
+  <h5><p style="color:white;font-weight:bold">Username:${userData.username}</p></h5>
+  <h5><p style="color:white;font-weight:bold">Fcoins:${userData.fcoins}</p></h5>
+  </div>`;
+}
+
+console.log(userData.imagen);
+console.log(info_p);
